fc488e9893f33df56f42207fae41619ae2c2d70e
Linneus <37879506+Linneus@users.noreply.github.com>
2025-07-28 13:50:39 -0700

    Update watch.lua

--- scripts/watch.lua
+++ scripts/watch.lua
@@ -1,8 +1,6 @@
 HOSTED_ITEMS = {
-"event_rowan", "event_oreburgh", "event_veilstone", "event_pastoria",
-"event_canalave", "event_saturn", "event_mars", "event_jupiter",
-"event_galactichq", "event_guardians", "event_distortion", "event_palpark",
-"event_cynthia", "event_bombs", "event_arceus", "event_warehouse"
+"regirock", "HallPrint", "ArcadePrint", "FactoryPrint",
+"CastlePrint"
 
 }
 
